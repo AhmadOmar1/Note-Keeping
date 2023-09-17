6af450013a3e58d5b7a4496f84c72b2816a5824d
Ahmad Omar <ahmadomarns@gmail.com>
2023-09-17 16:12:38 +0300

    note card component

--- client/src/components/noteCard/noteCard.js
+++ client/src/components/noteCard/noteCard.js
@@ -1,32 +1,36 @@
-import './noteCard.css'
-import React from 'react';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
-const NoteCard = ( { note }) => {
-    const formattedDate = new Date(note.creationDate).toLocaleString('en-US', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        hour: 'numeric',
-        minute: 'numeric'
-      });
-  return (
-    <div className='Card'>
-      <h3 className='title'>{note.title}</h3>
-      <p className='content'>{note.content}</p>
-      <p>Created at: <span>{formattedDate}</span></p> 
-    </div>
-  );
+const cardStyle = {
+  maxWidth: 300,
+  marginLeft: 0, // Ensure there are no unwanted margins
 };
 
-const NoteList = ({ notes }) => {
-    return (
-      <div className='NoteList'>
-        {notes.map(note => (
-          <NoteCard key={note.id} note={note}/>
-        ))}
-      </div>
-    );
-  };
-  
-  export default NoteList;
+const cardContentStyle = {
+    display: 'flex',
+    flexDirection:'column',
+    alignItems:'flex-start',
+    textAlign:'start'
+}
 
+
+export default function NoteCard() {
+  return (
+    <Card sx={cardStyle}>
+      <CardActionArea>
+        <CardContent sx={cardContentStyle}>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
