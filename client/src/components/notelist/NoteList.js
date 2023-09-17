b7afeff0e79d589103bfe25500521efb72f284e6
Ahmad Omar <ahmadomarns@gmail.com>
2023-09-17 16:10:46 +0300

    note list including all the cards

---
+++ client/src/components/notelist/NoteList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import NoteCard from '../notecard/NoteCard'
+import './NoteList.css'
+
+const NoteList = () => {
+  return (
+    <div className='node-list'>
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+
+
+
+    </div>
+  )
+}
+
+export default NoteList
