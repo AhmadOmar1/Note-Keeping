2183b79f5c794c2c7a0c3b240e57c4ec436e994f
Ahmad Omar <ahmadomarns@gmail.com>
2023-09-17 16:19:57 +0300

    update names

--- client/src/pages/home/HomePage.js
+++ client/src/pages/home/HomePage.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import NavBar from '../../components/navbar/NavBar'
-import { fetchNotes } from '../../services/NoteService';
-
-
+import NavBar from '../../components/navbar/navbar.js'
 import './HomePage.css';
 import FullWidthTextField from '../../components/fullWidthTextField/FullWidthTextField';
 import NoteList from '../../components/notelist/NoteList';
