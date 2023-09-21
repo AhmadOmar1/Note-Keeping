3cde6a724f9ea767264e1edfb32449a8a460b791
AhmadOmar <ahmadomarns@gmail.com>
2023-09-21 16:43:28 +0300

    alert componenet

---
+++ client/src/components/alert/AlertDialog.js
@@ -0,0 +1,42 @@
+import {react, useState}  from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function AlertDialog({ isOpen, onDelete }) {
+  const handleClose = (agree) => {
+    if(agree) {
+         onDelete();
+    }
+   
+  };
+
+
+  return (
+    <div>
+      <Dialog
+        open={isOpen} // Use the 'isOpen' prop directly
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Deleting the note"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to delete this note?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => (false)}>Disagree</Button>
+          <Button onClick={handleClose} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
