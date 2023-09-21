5de4c5157554f917bdaeec12172123f8cee0b306
AhmadOmar <ahmadomarns@gmail.com>
2023-09-21 16:43:07 +0300

    card style and component

--- client/src/components/noteCard/noteCard.js
+++ client/src/components/noteCard/noteCard.js
@@ -1,36 +1,125 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import './NoteCard.css';
+import { Box, TextField, IconButton, Button } from '@mui/material';
+import Fab from '@mui/material/Fab';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const cardStyle = {
-  maxWidth: 300,
-  marginLeft: 0, // Ensure there are no unwanted margins
+  backgroundColor: 'lightsteelblue',
+  width: '300px',
+  height: '180px',
+  cursor: 'pointer',
+  boxShadow: '0 1px 5px 1px',
 };
 
 const cardContentStyle = {
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  textAlign: 'start',
+  justifyContent: 'space-between',
+};
+
+export default function NoteCard({ id, title, content, creationDate, onDelete }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [isHover, setIsHover] = useState(false);
+  const [noteTitle, setTitle] = useState(title);
+  const [noteContent, setContent] = useState(content);
+
+
+  const toggleEditing = () => {
+    setIsEditing(!isEditing);
+  };
+
+  const handleClose = () => { };
+
+  const textFieldStyle = {
+    border: 'none',
+  };
+
+  const centerDivStyle = {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: 'white',
+    zIndex: 100,
+    width: '30%',
+    boxShadow: '0px 1px 5px 1px',
+    borderRadius: '5px',
+    padding: '20px',
     display: 'flex',
-    flexDirection:'column',
-    alignItems:'flex-start',
-    textAlign:'start'
-}
+    gap: '1rem',
+    flexDirection: 'column',
+  };
+
+  const handleMouseEnter = () => {
+    setIsHover(true);
+  }
+
+  const handleMouseLeave = () => {
+    setIsHover(false);
+
+  }
 
 
-export default function NoteCard() {
   return (
-    <Card sx={cardStyle}>
-      <CardActionArea>
-        <CardContent sx={cardContentStyle}>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <div>
+      {isEditing ? (
+        <div style={centerDivStyle}>
+          <TextField sx={textFieldStyle} fullWidth label="Note Title" id="noteTitle" value={noteTitle} />
+          <TextField
+            fullWidth
+            label="Note Content"
+            id="noteContent"
+            multiline
+            rows={4}
+            value={noteContent}
+          />
+          <div
+            style={{
+              display: 'flex',
+              alignSelf: 'flex-end',
+              gap: '0.6rem',
+            }}
+          >
+            <Button variant="outlined" color="success">
+              Update
+            </Button>
+            <Button variant="outlined" color="warning" onClick={toggleEditing}>
+              Close
+            </Button>
+          </div>
+        </div>
+      ) : (
+        <Card className='cardStyle' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}  >
+          <CardContent className='cardContentStyle' >
+            <div>
+              <Typography gutterBottom variant="h5" component="div">
+                {title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {content}
+              </Typography>
+            </div>
+
+            <div className='last-line'>
+              <span className="date-field">
+                {creationDate}
+              </span>
+              {isHover && (<IconButton color="primary" aria-label="delete" size='large' onClick={onDelete} >
+                <DeleteIcon />
+              </IconButton>)
+              }
+            </div>
+
+          </CardContent>
+        </Card>
+      )}
+    </div>
   );
 }
